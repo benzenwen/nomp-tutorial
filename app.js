df115d4649ff710c50f0a81cc486e1ab8272ccd4
Ben Wen <github@benwen.com>
2012-10-15 16:35:53 -0700

    Initial commit.

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+var mongo = require('mongodb');
+var mongoUri = process.env.MONGO_URI || 
+  process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/myfirstnode'; 
+console.log (mongoUri);
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+      collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+      });
+  });
+});
